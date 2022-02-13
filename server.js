d68c632595fe3b7cc0ec4cde4b090d054b61de75
alexkrushkova <alexkrushkova@gmail.com>
2022-02-13 12:39:45 +0100

    Finished author routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
 
 const indexRouter = require('./controllers/index')
 const authorRouter = require('./controllers/authors')
@@ -15,6 +16,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
@@ -23,8 +25,6 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
-
-
 
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
